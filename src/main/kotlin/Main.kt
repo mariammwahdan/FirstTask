d37edc811db1c5878bcdbe94291ef50e7b68b9a5
Mariam Wahdan <mariamwahdan32@gmail.com>
2022-08-05 02:24:32 +0200

    V3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
 fun main() {
-   val number1: Double =20.4
-    val number2: Float =3.4f
+   var number1 =20.4
+    var number2 =3.4f
     val result= number1+number2
     println(result)
+    number1= 10.0
+    number2=22.4f
+   val result2=number1+number2
+    println(result2)
 
 
 }
